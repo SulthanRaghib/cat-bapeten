0a2e21c24f9032a657e05923ed500c21a4706c6d
SulthanRaghib <raghib.smp@gmail.com>
2026-03-11 08:12:49 +0700

    feat: add favicon link to exam layout

--- resources/views/layouts/exam.blade.php
+++ resources/views/layouts/exam.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'CAT BAPETEN') }} - Ujian</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/logo.png') }}">
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;800&display=swap"
