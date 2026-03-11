5a39686f763b452ca2ae2f668f436728e1eb3b5b
SulthanRaghib <raghib.smp@gmail.com>
2026-03-11 08:12:44 +0700

    feat: replace DateTimePicker with CustomDateTimePicker in ExamPackageForm and register custom JS asset

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Support\Assets\Js;
+use Filament\Support\Facades\FilamentAsset;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         \App\Models\Question::observe(\App\Observers\QuestionObserver::class);
+
+        FilamentAsset::register([
+            Js::make('custom-date-time-picker', asset('js/custom-date-time-picker.js')),
+        ]);
     }
 }
